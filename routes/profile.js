c87dcb38ed08b71de84589a105f1328a98106804
apc <alexcurran.va@gmail.com>
2020-01-03 17:38:41 -0600

    Added vue client frontend setup

--- routes/profile.js
+++ routes/profile.js
@@ -9,9 +9,7 @@ router.get('/:platform/:gamertag', async (req, res) => {
         };
 
         const { platform, gamertag } = req.params;
-
         const response = await fetch(`${process.env.TRACKER_API_URL}/profile/${platform}/${gamertag}`, { headers });
-
         const data = await response.json();
 
         if (data.errors && data.errors.length > 0) {
