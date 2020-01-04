ed8ee8f9d7c35ad9a591b02b78427ca50b8445a7
apc <alexcurran.va@gmail.com>
2020-01-04 13:52:44 -0600

    Setup global styles for view routes

--- routes/profile.js
+++ routes/profile.js
@@ -4,12 +4,13 @@ const fetch = require("node-fetch");
 
 router.get('/:platform/:gamertag', async (req, res) => {
     try {
+        const { platform, gamertag } = req.params; // Grab params out for use in URL
+        const URL = `${process.env.TRACKER_API_URL}/profile/${platform}/${gamertag}`;
         const headers = {
             "TRN-Api-Key": process.env.TRACKER_API_KEY
         };
 
-        const { platform, gamertag } = req.params;
-        const response = await fetch(`${process.env.TRACKER_API_URL}/profile/${platform}/${gamertag}`, { headers });
+        const response = await fetch(URL, { headers });
         const data = await response.json();
 
         if (data.errors && data.errors.length > 0) {
