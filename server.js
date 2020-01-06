b2d93df98e826fc85fbee3ba9755be3f03838e11
apc <alexcurran.va@gmail.com>
2020-01-06 12:35:38 -0600

    Set node version for deployment

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ if (process.env.NODE_ENV === "development") {
 // Profile routes
 app.use("/api/v1/profile", require("./routes/profile"));
 
+// Handle production app
+if (process.env.NODE_ENV === "production") {
+    // Set static folder to public
+    app.use(express.static(__dirname + "/public/"));
+
+    // Handle Vue SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
